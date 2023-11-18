a4060c3955defb56e1515d2f243afeee571bb0c0
mjmaurijimenez <mj0388572@gmail.com>
2023-11-18 13:06:17 -0300

    commit 01

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -5,23 +5,37 @@ function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
 }
+ function nuevoString(meta){
+   console.log(meta);
+   
+ }
+ {
+   var string = "meta" 
+   console.log("meta")
+ }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
-}
+ }{ var x = 5;
+  var y = 12;
+   console.log(x);// 5
+   console.log(y);// 12
+
+   console.log( x + y);
+return x + y;}
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
-   // Tu código:
-}
+   // Tu código: 
+}   
 
-function divide(x, y) {
+ function divide(x, y){
    // Retorna el resultado de su división.
    // Tu código:
-}
+} 
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
